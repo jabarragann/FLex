e94b242c91db2231b53b9520e9149c9910a5b97e
Juan Antonio Barragan <jbarrag3@jh.edu>
2025-04-17 15:52:51 +0200

    refactor pc_from_depthanything script

--- JuanScripts/StructureFromMotionExp/pc_from_depthanything.py
+++ JuanScripts/StructureFromMotionExp/pc_from_depthanything.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import open3d as o3d
 
-# PC to RGBD Open3D
-# https://www.open3d.org/docs/latest/python_example/geometry/point_cloud/index.html#point-cloud-to-rgbd-py
-
 
 # Approximate intrinsic matrix
 def generate_camera_intrinsics(w, h, f=999):
@@ -88,17 +85,46 @@ def create_point_cloud_with_open3d(
 
     print(cam_model)
 
-    ## Project back to image
-    points = np.asarray(pcd.points)
-    colors = (np.asarray(pcd.colors) * 255).astype(np.uint8)  # shape (N, 3)
+    return pcd
 
-    img_from_pc = point_cloud_to_rgb_no_zbuffer(points, colors, K)
 
-    # Save or display
-    cv2.imwrite(
-        str(basedir / f"outputs/reprojected_rgb_no_zbuffer_{frame_id:04d}.png"),
-        cv2.cvtColor(img_from_pc, cv2.COLOR_RGB2BGR),
-    )
+def create_point_cloud_with_rays(
+    K: PinHoleCameraParams, rgb, depth, basedir, frame_id: int
+):
+    u, v = np.meshgrid(np.arange(K.W), np.arange(K.H))
+    u = u.flatten()
+    v = v.flatten()
+    depth = depth.flatten()
+
+    # Remove points with zero depth
+    valid = depth > 0
+    u, v, depth = u[valid], v[valid], depth[valid]
+
+    x = (u - K.cx) * depth / K.fx
+    y = (v - K.cy) * depth / K.fy
+    z = depth
+
+    points_3d = np.vstack((x, y, z)).T  # Shape: (N, 3)
+
+    colors = rgb[v, u] / 255.0  # Normalize to [0,1]
+
+    # Create Open3D PointCloud
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points_3d)
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    o3d.io.write_point_cloud(basedir / f"outputs/frame_{frame_id:04d}_pc_rays.ply", pcd)
+
+    return pcd
+
+    ## Optionally it could be implemented with matrix multiplication
+    # K = np.array([[fx, 0, cx],
+    #             [0, fy, cy],
+    #             [0,  0,  1]])
+    ## Construct homogeneous 2D pixel coordinates
+    # pixels_h = np.vstack((u, v, np.ones_like(u)))
+    # # Compute 3D points using inverse projection
+    # points_3d = np.linalg.inv(K) @ pixels_h * depth  # Shape: (3, N)
+    # points_3d = points_3d.T  # Convert to (N, 3)
 
 
 def visualize_rgb_and_depth(rgb, depth):
@@ -115,6 +141,45 @@ def visualize_rgb_and_depth(rgb, depth):
     ax[0, 1].axis("off")  # Turn off axis
 
     plt.show()
+
+
+def compare_projected_pc(K: PinHoleCameraParams, pc1, pc2, basedir, frame_id):
+    points = np.asarray(pc1.points)
+    colors = (np.asarray(pc1.colors) * 255).astype(np.uint8)  # shape (N, 3)
+    img_from_pc = point_cloud_to_rgb_no_zbuffer(points, colors, K)
+    # Save or display
+    cv2.imwrite(
+        str(basedir / f"outputs/projected_pc1_no_zbuffer_{frame_id:04d}.png"),
+        cv2.cvtColor(img_from_pc, cv2.COLOR_RGB2BGR),
+    )
+
+    points = np.asarray(pc2.points)
+    colors = (np.asarray(pc2.colors) * 255).astype(np.uint8)  # shape (N, 3)
+    img_from_pc = point_cloud_to_rgb_no_zbuffer(points, colors, K)
+    # Save or display
+    cv2.imwrite(
+        str(basedir / f"outputs/projected_pc2_no_zbuffer_{frame_id:04d}.png"),
+        cv2.cvtColor(img_from_pc, cv2.COLOR_RGB2BGR),
+    )
+
+    ## PC to RGBD with Open3D
+    ## https://www.open3d.org/docs/latest/python_example/geometry/point_cloud/index.html#point-cloud-to-rgbd-py
+    ## https://github.com/isl-org/Open3D/issues/2596
+    ## Didn't work => project_to_rgbd_image no available.
+
+    # intrinsic = o3d.core.Tensor(
+    #     ([[K.fx, 0, K.cx], [0, K.fy, K.cy], [0, 0, 1]])
+    # ).to(o3d.core.Dtype.Float32)
+    # rgbd_reproj = pc1.project_to_rgbd_image(
+    #     intrinsic, K.W, K.H, depth_scale=1.0, depth_trunc=3.0
+    # )
+    # cv2.imwrite(
+    #     str(
+    #         basedir
+    #         / f"outputs/projected_pc1_no_zbuffer_{rgbd_reproj.color:04d}_o3d.png"
+    #     ),
+    #     cv2.cvtColor(img_from_pc, cv2.COLOR_RGB2BGR),
+    # )
 
 
 def main():
@@ -146,7 +211,10 @@ def main():
     )
 
     visualize_rgb_and_depth(rgb, depth)
-    create_point_cloud_with_open3d(cam_params, rgb, depth, basedir, frame_id)
+    pc1 = create_point_cloud_with_open3d(cam_params, rgb, depth, basedir, frame_id)
+    pc2 = create_point_cloud_with_rays(cam_params, rgb, depth, basedir, frame_id)
+
+    compare_projected_pc(cam_params, pc1, pc2, basedir, frame_id)
 
     # print(pcd)
     print(rgb.shape)
