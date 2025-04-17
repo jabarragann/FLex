40ed84395a6f2435ef89a7cef05c5d091cfb48eb
Juan Antonio Barragan <jbarrag3@jh.edu>
2025-04-17 13:26:18 +0200

    refactor pc_from_depthanything script

--- JuanScripts/StructureFromMotionExp/pc_from_depthanything.py
+++ JuanScripts/StructureFromMotionExp/pc_from_depthanything.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import cv2
@@ -23,26 +24,39 @@ def generate_camera_intrinsics(w, h, f=999):
     return K
 
 
-def point_cloud_to_rgb_no_zbuffer(points, colors, W, H, fx, fy, cx, cy):
+@dataclass
+class PinHoleCameraParams:
+    fx: float
+    fy: float
+    cx: float
+    cy: float
+    W: float
+    H: float
+
+
+def point_cloud_to_rgb_no_zbuffer(
+    points: np.ndarray, colors: np.ndarray, K: PinHoleCameraParams
+):
     # Filter out points behind the camera
     valid = points[:, 2] > 0
     points = points[valid]
 
-    u = (points[:, 0] * fx / points[:, 2]) + cx
-    v = (points[:, 1] * fy / points[:, 2]) + cy
+    # Projection to image plane
+    u = (points[:, 0] * K.fx / points[:, 2]) + K.cx
+    v = (points[:, 1] * K.fy / points[:, 2]) + K.cy
 
     # Round to get pixel coordinates
     u = u.astype(np.int32)
     v = v.astype(np.int32)
 
     # Optional: filter points that fall outside image bounds
-    mask = (u >= 0) & (u < W) & (v >= 0) & (v < H)
+    mask = (u >= 0) & (u < K.W) & (v >= 0) & (v < K.H)
     u = u[mask]
     v = v[mask]
     colors = colors[mask]
 
     # Initialize image
-    rgb_img = np.zeros((H, W, 3), dtype=np.uint8)
+    rgb_img = np.zeros((K.H, K.W, 3), dtype=np.uint8)
 
     # Assign color values (overwriting if necessary)
     rgb_img[v, u] = colors
@@ -50,13 +64,17 @@ def point_cloud_to_rgb_no_zbuffer(points, colors, W, H, fx, fy, cx, cy):
     return rgb_img
 
 
-def create_point_cloud_with_open3d(K, rgb, depth, W, H, basedir):
-    fx = K[0, 0]
-    fy = K[1, 1]
-    cx = K[0, 2]
-    cy = K[1, 2]
+def create_point_cloud_with_open3d(
+    K: PinHoleCameraParams, rgb, depth, basedir, frame_id: int
+):
+    # fx = K[0, 0]
+    # fy = K[1, 1]
+    # cx = K[0, 2]
+    # cy = K[1, 2]
 
-    cam_model = o3d.camera.PinholeCameraIntrinsic(int(W), int(H), fx, fy, cx, cy)
+    cam_model = o3d.camera.PinholeCameraIntrinsic(
+        int(K.W), int(K.H), K.fx, K.fy, K.cx, K.cy
+    )
 
     rgb_im = o3d.geometry.Image(rgb)
     depth_im = o3d.geometry.Image(depth)
@@ -66,7 +84,7 @@ def create_point_cloud_with_open3d(K, rgb, depth, W, H, basedir):
     )
     pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, cam_model)
 
-    o3d.io.write_point_cloud(basedir / f"outputs/frame_{14:06d}_pc.ply", pcd)
+    o3d.io.write_point_cloud(basedir / f"outputs/frame_{frame_id:04d}_pc.ply", pcd)
 
     print(cam_model)
 
@@ -74,11 +92,11 @@ def create_point_cloud_with_open3d(K, rgb, depth, W, H, basedir):
     points = np.asarray(pcd.points)
     colors = (np.asarray(pcd.colors) * 255).astype(np.uint8)  # shape (N, 3)
 
-    img_from_pc = point_cloud_to_rgb_no_zbuffer(points, colors, W, H, fx, fy, cx, cy)
+    img_from_pc = point_cloud_to_rgb_no_zbuffer(points, colors, K)
 
     # Save or display
     cv2.imwrite(
-        str(basedir / "outputs/reprojected_rgb_no_zbuffer.png"),
+        str(basedir / f"outputs/reprojected_rgb_no_zbuffer_{frame_id:04d}.png"),
         cv2.cvtColor(img_from_pc, cv2.COLOR_RGB2BGR),
     )
 
@@ -102,28 +120,33 @@ def visualize_rgb_and_depth(rgb, depth):
 def main():
     basedir = Path(__file__).resolve().parent
 
-    # Load images
-    rgb = imageio.imread(basedir / "./sample_data/pc_depth_anything/frame_014.png")
-    # depth = imageio.imread(basedir/"./sample_data/pc_depth_anything/frame_014_depth.png")
-    disparity = np.load(
-        "/home/juan95/research/monocular_depth_est/Video-Depth-Anything/outputs_npz/cafiero_03.04_clip4_good_depths.npz"
-    )["depths"][13]
-
-    disparity_max = np.max(disparity)
-    disparity_min = np.min(disparity)
+    frame_id = 14  # 379 or 14
+    rgb_path = f"./sample_data/pc_depth_anything/frame_{frame_id:04d}.png"
+    depth_path = f"./sample_data/pc_depth_anything/depth_{frame_id:04d}.png"
+    rgb = imageio.imread(basedir / rgb_path)
+    disparity = imageio.imread(basedir / depth_path)
+    disparity = disparity.astype(np.float32)
     disparity_normalized = (
-        (disparity - disparity_min) / (disparity_max - disparity_min)
-    ) + 1.0  # Normalize to [0, 1]
+        disparity / 65535.0
+    ) + 1  # Normalize to [1, 2] to avoid div by 0.
 
-    depth = 1000 / disparity_normalized
-    # depth = disparity
+    scale = 1000  # Scale to see mesh in Open3D
+    depth = scale / disparity_normalized
 
     W = rgb.shape[1]
     H = rgb.shape[0]
     K = generate_camera_intrinsics(W, H)
+    cam_params = PinHoleCameraParams(
+        fx=K[0, 0],
+        fy=K[1, 1],
+        cx=K[0, 2],
+        cy=K[1, 2],
+        W=W,
+        H=H,
+    )
 
     visualize_rgb_and_depth(rgb, depth)
-    create_point_cloud_with_open3d(K, rgb, depth, W, H, basedir)
+    create_point_cloud_with_open3d(cam_params, rgb, depth, basedir, frame_id)
 
     # print(pcd)
     print(rgb.shape)
